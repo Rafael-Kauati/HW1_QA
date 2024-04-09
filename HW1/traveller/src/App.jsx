e813302f3c5e8e547fe7f8a905970af8c96ed0ea
TK <kauati.toiti@gmail.com>
2024-04-09 12:51:36 +0100

    Changes 6

--- HW1/traveller/src/App.jsx
+++ HW1/traveller/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import './App.css';
 import BookingForm from './components/BookingForm/BookingForm';
 import axios from 'axios';
